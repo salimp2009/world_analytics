4935dfc8ea620fc4142fcf43bb56d54e512d23d8
salimp2009@gmail.com <salimp2009@gmail.com>
2023-03-18 21:25:06 +0300

    added a css style to change the color of twitter and fb counts below zero and added word count

--- script.js
+++ script.js
@@ -4,12 +4,34 @@ const twitterNumberEl = document.querySelector(".stat__number--twitter");
 const facebookNumberEl = document.querySelector(".stat__number--facebook");
 const wordsNumberEl = document.querySelector(".stat__number--words");
 
+const twitterCharLimit = 200;
+const facebookCharLimit = 2200;
+
 textareaEl.addEventListener("input", () => {
   const numberOfCharacters = textareaEl.value.length;
-  const twitterCharsLeft = 280 - numberOfCharacters;
-  const facebookCharsLeft = 2200 - numberOfCharacters;
+  const twitterCharsLeft = twitterCharLimit - numberOfCharacters;
+  const facebookCharsLeft = facebookCharLimit - numberOfCharacters;
+  let numberOfWords = 0;
+  if (textareaEl.value.trim() === "" || textareaEl.value === "") {
+    numberOfWords = 0;
+  } else {
+    numberOfWords = textareaEl.value.trim().split(" ").length;
+  }
+
+  if (twitterCharsLeft < 0) {
+    twitterNumberEl.classList.add("stat__number--limit");
+  } else if (twitterNumberEl.classList.contains("stat__number--limit")) {
+    twitterNumberEl.classList.remove("stat__number--limit");
+  }
+
+  if (facebookCharsLeft < 0) {
+    facebookNumberEl.classList.add("stat__number--limit");
+  } else if (facebookNumberEl.classList.contains("stat__number--limit")) {
+    facebookNumberEl.classList.remove("stat__number--limit");
+  }
 
   characterNumberEl.textContent = numberOfCharacters;
   twitterNumberEl.textContent = twitterCharsLeft;
   facebookNumberEl.textContent = facebookCharsLeft;
+  wordsNumberEl.textContent = numberOfWords;
 });
