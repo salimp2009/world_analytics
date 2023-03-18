dd9fb533bd5e7baf6edd1bddb44c31451c21abb2
salimp2009@gmail.com <salimp2009@gmail.com>
2023-03-18 15:44:08 +0300

    added js code to count the characters, twitter, facebook counts; next is to change the color if exceeds limit values and add word count as well

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-const textareaEl = document.querySelector("textarea");
+const textareaEl = document.querySelector(".textarea");
+const characterNumberEl = document.querySelector(".stat__number--characters");
+const twitterNumberEl = document.querySelector(".stat__number--twitter");
+const facebookNumberEl = document.querySelector(".stat__number--facebook");
+const wordsNumberEl = document.querySelector(".stat__number--words");
+
 textareaEl.addEventListener("input", () => {
   const numberOfCharacters = textareaEl.value.length;
+  const twitterCharsLeft = 280 - numberOfCharacters;
+  const facebookCharsLeft = 2200 - numberOfCharacters;
+
+  characterNumberEl.textContent = numberOfCharacters;
+  twitterNumberEl.textContent = twitterCharsLeft;
+  facebookNumberEl.textContent = facebookCharsLeft;
 });
