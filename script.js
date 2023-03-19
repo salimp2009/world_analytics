15e5ed7d6ac2337cf6fa1f3c418a316252724aa5
salimp2009@gmail.com <salimp2009@gmail.com>
2023-03-19 12:36:01 +0300

    added a check for security to prevent <script> to be inserted; used regex pattern to allow line breaks and also replace it with empty string and alert ; project completed

--- script.js
+++ script.js
@@ -6,17 +6,28 @@ const wordsNumberEl = document.querySelector(".stat__number--words");
 
 const twitterCharLimit = 200;
 const facebookCharLimit = 2200;
+const scriptPattern = /<[\s\S]*?script[\s\S]*?>/i;
 
-textareaEl.addEventListener("input", () => {
-  const numberOfCharacters = textareaEl.value.length;
-  const twitterCharsLeft = twitterCharLimit - numberOfCharacters;
-  const facebookCharsLeft = facebookCharLimit - numberOfCharacters;
+function hasScript(textarea) {
+  return scriptPattern.test(textarea.value);
+}
+
+const inputHandler = () => {
+  if (hasScript(textareaEl) || textareaEl.value.includes("<script>")) {
+    alert("<script> not allowed!");
+    textareaEl.value = textareaEl.value.replace(scriptPattern, " ");
+  }
+
   let numberOfWords = 0;
   if (textareaEl.value.trim() === "" || textareaEl.value === "") {
     numberOfWords = 0;
   } else {
     numberOfWords = textareaEl.value.trim().split(" ").length;
   }
+
+  const numberOfCharacters = textareaEl.value.length;
+  const twitterCharsLeft = twitterCharLimit - numberOfCharacters;
+  const facebookCharsLeft = facebookCharLimit - numberOfCharacters;
 
   if (twitterCharsLeft < 0) {
     twitterNumberEl.classList.add("stat__number--limit");
@@ -34,4 +45,6 @@ textareaEl.addEventListener("input", () => {
   twitterNumberEl.textContent = twitterCharsLeft;
   facebookNumberEl.textContent = facebookCharsLeft;
   wordsNumberEl.textContent = numberOfWords;
-});
+};
+
+textareaEl.addEventListener("input", inputHandler);
